b2f938aefd769d5d1efcab8e51ee4ce273c836ac
Papokencode <pavelsenator0@gmail.com>
2023-02-05 19:52:33 +0300

    fix : added models

--- breaks/admin.py
+++ breaks/admin.py
@@ -1,12 +1,45 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 
-from breaks.models import organisations
+from breaks.models import organisations, groups, replacements
 
+##########################
+# INLINES
+##########################
+class ReplacementEmployeeInline(TabularInline):
+    model = replacements.ReplacementEmployee
+    fields = ('employee', 'status')
+
+#StackedInline
+
+
+
+#########################
+# MODELS
+#########################
 @admin.register(organisations.Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'director',)
 
 
-@admin.register(organisations.Group)
+@admin.register(groups.Group)
 class OrganisationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'manager', 'min_active',)
+
+
+@admin.register(replacements.Replacement)
+class ReplacementAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'group', 'break_start', 'break_end', 'break_max_duration',
+    )
+
+    inlines = (
+        ReplacementEmployeeInline,
+    )
+
+
+@admin.register(replacements.ReplacementStatus)
+class ReplacementStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'code', 'name', 'sort', 'is_active',
+    )
